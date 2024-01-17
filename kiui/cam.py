a9a955ffd4ba150a4dc97069a229963d72148c01
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2024-01-17 15:53:03 +0900

    Update cam.py

--- kiui/cam.py
+++ kiui/cam.py
@@ -231,7 +231,7 @@ class OrbitCamera:
         self.fovy = np.deg2rad(fovy)  # deg 2 rad
         self.near = near
         self.far = far
-        self.center = np.array([0, 1.2, 0], dtype=np.float32)  # look at this point
+        self.center = np.array([0, 0.8, 0], dtype=np.float32)  # look at this point
         self.rot = Rotation.from_matrix(np.eye(3))
         self.up = np.array([0, 1, 0], dtype=np.float32)  # need to be normalized!
 
