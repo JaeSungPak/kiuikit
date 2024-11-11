94813af6ed4e21c04f18cd193037f9a7471fc007
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2024-11-11 22:27:37 +0900

    Update clip_sim.py

--- kiui/cli/clip_sim.py
+++ kiui/cli/clip_sim.py
@@ -79,10 +79,10 @@ if __name__ == '__main__':
     # render from random views and evaluate similarity
     results = []
     results_lpips_vgg = []
-    results_lpips_alex = []
+    # results_lpips_alex = []
 
     loss_fn_vgg = lpips.LPIPS(net='vgg')
-    loss_fn_alex = lpips.LPIPS(net='alex')
+    # loss_fn_alex = lpips.LPIPS(net='alex')
 
     elevation = [opt.elevation,]
     azimuth = np.linspace(0, 360, opt.num_azimuth, dtype=np.int32, endpoint=False)
@@ -106,16 +106,16 @@ if __name__ == '__main__':
             image_lpips = torch.from_numpy(image).permute(2, 0, 1)
 
             lpips_value_vgg = loss_fn_vgg(ref_img_lpips, image_lpips)
-            lpips_value_alex = loss_fn_alex(ref_img_lpips, image_lpips)
+            #lpips_value_alex = loss_fn_alex(ref_img_lpips, image_lpips)
 
             results_lpips_vgg.append(lpips_value_vgg[0][0][0][0].detach().numpy())
-            results_lpips_alex.append(lpips_value_alex[0][0][0][0].detach().numpy())
+            #results_lpips_alex.append(lpips_value_alex[0][0][0][0].detach().numpy())
 
             # import pdb; pdb.set_trace()
     
     avg_similarity = np.mean(results)
     avg_lpips_vgg = np.mean(results_lpips_vgg)
-    avg_lpips_alex = np.mean(results_lpips_alex)
+    #avg_lpips_alex = np.mean(results_lpips_alex)
 
     f = open("clip_s.txt", 'a', encoding="utf8")
     f.write(f"{avg_similarity}\n")
@@ -125,12 +125,12 @@ if __name__ == '__main__':
     f.write(f"{avg_lpips_vgg}\n")
     f.close() 
 
-    f = open("lpips_alex.txt", 'a', encoding="utf8")
-    f.write(f"{avg_lpips_alex}\n")
-    f.close() 
+    #f = open("lpips_alex.txt", 'a', encoding="utf8")
+    #f.write(f"{avg_lpips_alex}\n")
+    #f.close() 
 
     print(f"CLIP-S: {avg_similarity}")
     print(f"LPIPS_VGG: {avg_lpips_vgg}")
-    print(f"LPIPS_ALEX: {avg_lpips_alex}")
+    #print(f"LPIPS_ALEX: {avg_lpips_alex}")
 
             
